31882642ab4958029e6ebc1920cd0c45a4f84545
Gonzalo Cataldo <gonzalo.cataldo@valtech.com>
2023-11-03 16:23:55 -0300

    asd

--- build/server.js
+++ build/server.js
@@ -3,12 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv").config();
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello hola!");
 });
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
+require("dotenv").config();
 import express from 'express'
 const app = express()
 const PORT = process.env.PORT
 app.use(express.json())
 app.get("/", (req, res) => {
-  res.send("Hello World!")
+  res.send("Hello hola!")
 })
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
